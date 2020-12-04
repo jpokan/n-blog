ecbeb57d41d7430e7d222d456e9601fae09c3f07
Jorge Ou <8276144+Topomon@users.noreply.github.com>
2020-12-04 14:34:30 -0500

    wip: added hero links with magnet headings

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <div></div>
+  <div
+    class="grid grid-cols-1 lg:grid-cols-3 max-w-screen-lg mx-auto gap-5 py-20"
+  >
+    <div class="lg:col-span-2 rounded-lg">
+      <LinkMagnet
+        text="FRONT END"
+        class="font-bold text-5xl sm:text-6xl text-gray-800 dark: dark:hover:text-yellow-500 hover:bg-pink-500 dark:hover:bg-yellow-500 px-5"
+      />
+      <LinkMagnet
+        text=" DEVELOPER"
+        class="font-bold text-5xl sm:text-6xl text-gray-800 dark: dark:hover:text-yellow-500 hover:bg-pink-500 dark:hover:bg-yellow-500 px-5"
+      />
+      <LinkMagnet
+        text="+ ARCHITECT,"
+        class="font-bold text-5xl sm:text-6xl text-gray-300 hover:bg-pink-500 dark:hover:bg-yellow-500 px-5"
+      />
+      <LinkMagnet
+        text="DESIGNER"
+        class="font-bold text-5xl sm:text-6xl text-gray-300 hover:bg-pink-500 dark:hover:bg-yellow-500 px-5"
+      />
+    </div>
+    <div class="bg-pink-500 dark:bg-yellow-500 rounded-lg p-5">
+      Jorge Po-Kan
+    </div>
+  </div>
 </template>
 
 <script>
