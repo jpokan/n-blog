c96ae6feeaaa9a93fe955f1e8037fb1ddedc8742
Jorge Ou <8276144+Topomon@users.noreply.github.com>
2022-03-21 17:37:11 -0500

    removed highlight.js

--- components/Blog/BlogPost.vue
+++ components/Blog/BlogPost.vue
@@ -1,99 +1,76 @@
 <template>
-  <div v-editable="blok" class="max-w-screen-md mx-auto">
-    <BlogHeader class="text-gray-400" title="Blog Post" />
-    <div v-if="blok.thumbnail.filename" class="mb-5">
-      <img
-        :class="
-          blok.thumbnail_position === 'top'
-            ? 'object-top'
-            : blok.thumbnail_position === 'bottom'
-            ? 'object-bottom'
-            : 'object-center'
-        "
-        class="rounded-lg min-w-full max-h-60 sm:max-h-80 md:max-h-96 object-cover"
-        :src="blok.thumbnail.filename"
-        :alt="blok.thumbnail.alt"
-      />
-    </div>
-    <h1
-      class="rounded-md md:px-5 md:pb-2 md:pt-10 text-3xl md:text-5xl font-semibold text-pink-500 dark:text-yellow-500"
-    >
-      {{ title }}
-    </h1>
-    <p
-      v-if="lastPublished"
-      class="py-2 md:px-5 text-gray-500 dark:text-gray-300"
-    >
-      Last updated:
-      {{
-        new Date(lastPublished).toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })
-      }}
-    </p>
-    <div class="flex flex-row md:px-5">
-      <span
-        v-for="tag in tags"
-        :key="tag"
-        class="px-1 bg-gray-200 rounded dark:bg-gray-800 mr-2"
-        >{{ tag }}</span
-      >
-    </div>
-    <p v-if="blok.summary" class="md:p-5 md:py-10 py-10 md:text-lg">
-      <span class="font-bold">Summary:</span> {{ blok.summary }}
-    </p>
-    <div
-      v-if="blok.markdown"
-      ref="Post"
-      class="md:p-5 md:py-10 min-w-full prose max-w-none md:prose-lg dark:prose-dark rounded-lg"
-      v-html="$md.render(blok.markdown)"
-    ></div>
-    <BlogBackButton class="md:hidden" />
-    <div class="h-5 my-4"></div>
-  </div>
+	<div v-editable="blok" class="max-w-screen-md mx-auto">
+		<BlogHeader class="text-gray-400" title="Blog Post" />
+		<div v-if="blok.thumbnail.filename" class="mb-5">
+			<img
+				:class="
+					blok.thumbnail_position === 'top'
+						? 'object-top'
+						: blok.thumbnail_position === 'bottom'
+						? 'object-bottom'
+						: 'object-center'
+				"
+				class="rounded-lg min-w-full max-h-60 sm:max-h-80 md:max-h-96 object-cover"
+				:src="blok.thumbnail.filename"
+				:alt="blok.thumbnail.alt"
+			/>
+		</div>
+		<h1
+			class="rounded-md md:px-5 md:pb-2 md:pt-10 text-3xl md:text-5xl font-semibold text-pink-500 dark:text-yellow-500"
+		>
+			{{ title }}
+		</h1>
+		<p v-if="lastPublished" class="py-2 md:px-5 text-gray-500 dark:text-gray-300">
+			Last updated:
+			{{
+				new Date(lastPublished).toLocaleDateString('en-US', {
+					year: 'numeric',
+					month: 'long',
+					day: 'numeric'
+				})
+			}}
+		</p>
+		<div class="flex flex-row md:px-5">
+			<span v-for="tag in tags" :key="tag" class="px-1 bg-gray-200 rounded dark:bg-gray-800 mr-2">{{ tag }}</span>
+		</div>
+		<p v-if="blok.summary" class="md:p-5 md:py-10 py-10 md:text-lg">
+			<span class="font-bold">Summary:</span> {{ blok.summary }}
+		</p>
+		<div
+			v-if="blok.markdown"
+			ref="Post"
+			class="md:p-5 md:py-10 min-w-full prose max-w-none md:prose-lg dark:prose-dark rounded-lg"
+			v-html="$md.render(blok.markdown)"
+		></div>
+		<BlogBackButton class="md:hidden" />
+		<div class="h-5 my-4"></div>
+	</div>
 </template>
 
 <script>
-export default {
-  props: {
-    blok: {
-      type: Object,
-      required: true,
-    },
-    lastPublished: {
-      type: String,
-      default: 'Published date',
-    },
-    title: {
-      type: String,
-      default: 'Title',
-    },
-    tags: {
-      type: Array,
-      default: () => [],
-    },
-  },
-  mounted() {
-    this.styleCode()
-  },
-  updated() {
-    this.styleCode()
-  },
-  methods: {
-    styleCode() {
-      // Highlight code blocks if exists
-      const post = this.$refs.Post
-      if (post !== undefined) {
-        const codeArray = post.querySelectorAll('code')
-        codeArray.forEach((element) => {
-          this.$highlight(element)
-        })
-      }
-    },
-  },
-}
+	export default {
+		props: {
+			blok: {
+				type: Object,
+				required: true
+			},
+			lastPublished: {
+				type: String,
+				default: 'Published date'
+			},
+			title: {
+				type: String,
+				default: 'Title'
+			},
+			tags: {
+				type: Array,
+				default: () => []
+			}
+		},
+		mounted() {},
+		updated() {},
+		methods: {}
+	}
 </script>
 
 <style scoped></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,156 +1,156 @@
 export default {
-  server: {
-    port: 3000, // default: 3000
-    host: '0.0.0.0',
-  },
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+	server: {
+		port: 3000, // default: 3000
+		host: '0.0.0.0'
+	},
+	// Target (https://go.nuxtjs.dev/config-target)
+	target: 'static',
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'Jpokan',
-    htmlAttrs: {
-      lang: 'en',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content:
-          'Personal website, blog, and portfolio made with Nuxt.js, Vue.js, TailwindCSS and other cool libraries.',
-      },
+	// Global page headers (https://go.nuxtjs.dev/config-head)
+	head: {
+		title: 'Jpokan',
+		htmlAttrs: {
+			lang: 'en'
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				hid: 'description',
+				name: 'description',
+				content:
+					'Personal website, blog, and portfolio made with Nuxt.js, Vue.js, TailwindCSS and other cool libraries.'
+			},
 
-      // Twitter
-      // Test on: https://cards-dev.twitter.com/validator
-      {
-        hid: 'twitter:card',
-        name: 'twitter:card',
-        content: 'summary_large_image',
-      },
-      { hid: 'twitter:site', name: 'twitter:site', content: 'Jpokan' },
-      {
-        hid: 'twitter:url',
-        name: 'twitter:url',
-        content: 'https://jpokan.netlify.app',
-      },
-      {
-        hid: 'twitter:title',
-        name: 'twitter:title',
-        content: 'Jpokan',
-      },
-      {
-        hid: 'twitter:description',
-        name: 'twitter:description',
-        content:
-          'Personal website, blog, and portfolio made with Nuxt.js, Vue.js, TailwindCSS and other cool libraries.',
-      },
-      {
-        hid: 'twitter:image',
-        name: 'twitter:image',
-        content:
-          'https://a.storyblok.com/f/98358/800x407/5f03585a87/jpokan800.gif',
-      },
+			// Twitter
+			// Test on: https://cards-dev.twitter.com/validator
+			{
+				hid: 'twitter:card',
+				name: 'twitter:card',
+				content: 'summary_large_image'
+			},
+			{ hid: 'twitter:site', name: 'twitter:site', content: 'Jpokan' },
+			{
+				hid: 'twitter:url',
+				name: 'twitter:url',
+				content: 'https://jpokan.netlify.app'
+			},
+			{
+				hid: 'twitter:title',
+				name: 'twitter:title',
+				content: 'Jpokan'
+			},
+			{
+				hid: 'twitter:description',
+				name: 'twitter:description',
+				content:
+					'Personal website, blog, and portfolio made with Nuxt.js, Vue.js, TailwindCSS and other cool libraries.'
+			},
+			{
+				hid: 'twitter:image',
+				name: 'twitter:image',
+				content: 'https://a.storyblok.com/f/98358/800x407/5f03585a87/jpokan800.gif'
+			},
 
-      // Open Graph
-      // Test on: https://developers.facebook.com/tools/debug/
-      { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt' },
-      { hid: 'og:type', property: 'og:type', content: 'website' },
-      {
-        hid: 'og:url',
-        property: 'og:url',
-        content: 'https://jpokan.netlify.app',
-      },
-      {
-        hid: 'og:title',
-        property: 'og:title',
-        content: 'Jpokan',
-      },
-      {
-        hid: 'og:description',
-        property: 'og:description',
-        content:
-          'Personal website, blog, and portfolio made with Nuxt.js, Vue.js, TailwindCSS and other cool libraries.',
-      },
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content:
-          'https://a.storyblok.com/f/98358/800x407/5f03585a87/jpokan800.gif',
-      },
-      {
-        hid: 'og:image:secure_url',
-        property: 'og:image:secure_url',
-        content:
-          'https://a.storyblok.com/f/98358/800x407/5f03585a87/jpokan800.gif',
-      },
-      {
-        hid: 'og:image:alt',
-        property: 'og:image:alt',
-        content: 'Jpokan',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
+			// Open Graph
+			// Test on: https://developers.facebook.com/tools/debug/
+			{ hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt' },
+			{ hid: 'og:type', property: 'og:type', content: 'website' },
+			{
+				hid: 'og:url',
+				property: 'og:url',
+				content: 'https://jpokan.netlify.app'
+			},
+			{
+				hid: 'og:title',
+				property: 'og:title',
+				content: 'Jpokan'
+			},
+			{
+				hid: 'og:description',
+				property: 'og:description',
+				content:
+					'Personal website, blog, and portfolio made with Nuxt.js, Vue.js, TailwindCSS and other cool libraries.'
+			},
+			{
+				hid: 'og:image',
+				property: 'og:image',
+				content: 'https://a.storyblok.com/f/98358/800x407/5f03585a87/jpokan800.gif'
+			},
+			{
+				hid: 'og:image:secure_url',
+				property: 'og:image:secure_url',
+				content: 'https://a.storyblok.com/f/98358/800x407/5f03585a87/jpokan800.gif'
+			},
+			{
+				hid: 'og:image:alt',
+				property: 'og:image:alt',
+				content: 'Jpokan'
+			}
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+	},
 
-  generate: {
-    fallback: '404.html',
-  },
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '@/assets/css/fonts.css', // Serve custom fonts from server.
-    '@/assets/css/tailwind.css',
-  ],
-  router: {
-    linkActiveClass: 'dark:text-yellow-500 text-pink-500', // using tailwind styles
-  },
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/gsap-extras.js', '~/plugins/highlightjs.js'],
+	generate: {
+		fallback: '404.html'
+	},
+	// Global CSS (https://go.nuxtjs.dev/config-css)
+	css: [
+		'@/assets/css/fonts.css', // Serve custom fonts from server.
+		'@/assets/css/tailwind.css'
+	],
+	router: {
+		linkActiveClass: 'dark:text-yellow-500 text-pink-500' // using tailwind styles
+	},
+	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+	plugins: ['~/plugins/gsap-extras.js'],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
-  path: '~/components/Svg/',
-  ignore: ['**/Animate/*.vue'], // Except this folder
+	// Auto import components (https://go.nuxtjs.dev/config-components)
+	components: true,
+	path: '~/components/Svg/',
+	ignore: ['**/Animate/*.vue'], // Except this folder
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    // https://color-mode.nuxtjs.org
-    '@nuxtjs/color-mode',
-  ],
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    [
-      '@nuxtjs/markdownit',
-      {
-        html: true,
-        injected: true,
-        use: [
-          'markdown-it-attrs',
-          // 'markdown-it-highlightjs'
-        ],
-      },
-    ],
-    [
-      'storyblok-nuxt',
-      {
-        accessToken: process.env.STORYBLOK_SPACE_PREVIEW_TOKEN,
-        cache: 'auto',
-        cacheProvider: 'memory',
-      },
-    ],
-  ],
-  // Disable @nuxt/color-mode classSuffix to work with TailwindCSS 1.8+
-  colorMode: {
-    classSuffix: '',
-  },
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+	buildModules: [
+		// https://go.nuxtjs.dev/tailwindcss
+		'@nuxtjs/tailwindcss',
+		// https://color-mode.nuxtjs.org
+		'@nuxtjs/color-mode'
+	],
+	tailwindcss: {
+		viewer: false
+	},
+	// Modules (https://go.nuxtjs.dev/config-modules)
+	modules: [
+		[
+			'@nuxtjs/markdownit',
+			{
+				html: true,
+				injected: true,
+				use: [
+					'markdown-it-attrs'
+					// 'markdown-it-highlightjs'
+				]
+			}
+		],
+		[
+			'storyblok-nuxt',
+			{
+				accessToken: process.env.STORYBLOK_SPACE_PREVIEW_TOKEN,
+				cache: 'auto',
+				cacheProvider: 'memory'
+			}
+		]
+	],
+	// Disable @nuxt/color-mode classSuffix to work with TailwindCSS 1.8+
+	colorMode: {
+		classSuffix: ''
+	},
+	// Axios module configuration (https://go.nuxtjs.dev/config-axios)
+	axios: {},
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-    transpile: ['gsap', 'highlight.js'],
-  },
+	// Build Configuration (https://go.nuxtjs.dev/config-build)
+	build: {
+		transpile: ['gsap', 'highlight.js']
+	}
 }
